9ff434419b2a2be9d3dfe69544bda187d39d1c9f
Fernando Vega <fvegagiga@gmail.com>
2026-05-24 17:12:27 +0200

    Initial Commit

---
+++ RickMortyPersistImage/RickMortyPersistImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyPersistImageApp.swift
+//  RickMortyPersistImage
+//
+//  Created by Fernando Vega on 24/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyPersistImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
